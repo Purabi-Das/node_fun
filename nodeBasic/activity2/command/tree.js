6a088e8539aa52cdde931f98e1bcb90738d95e33
Purabi Das <purabid2001@gmail.com>
2022-10-18 00:54:24 +0530

    tree file updated

--- nodeBasic/activity2/command/tree.js
+++ nodeBasic/activity2/command/tree.js
@@ -1,5 +1,23 @@
-function treeFn(src) {
-    console.log("tree command executed with", src);
+let fs = require('fs');
+let path = require('path');
+
+function treeFn(srcPath) {
+    // console.log("tree command executed with", src);
+
+    if(srcPath == undefined) 
+        srcPath = process.cwd();
+
+        let content = fs.readdirSync(srcPath);
+        // console.log(content);
+
+        let parentFolder = path.basename(srcPath);
+
+        let completePath = "|--" + parentFolder 
+
+        for(let i = 0; i < content.length; i++) {
+            completePath = completePath + "\n\t" + "|——" + content[i];
+        }
+        console.log(completePath);
 }
 
 module.exports = {
